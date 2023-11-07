3e3f9349a9556504d21078ceb54c4b4281d6ce53
br1_dev <magaro.bruno@gmail.com>
2023-11-06 23:01:40 -0300

    data && style playlist datail init

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css';
 import { NavControl } from './components/NavControl';
 import { PlaylistNav } from './components/PlaylistNav';
+import data from '../src/data.js';
 function App() {
+	//console.log(data);
 	return (
 		<div>
 			<PlaylistNav />
-			<NavControl />
+
+			{/*<NavControl />*/}
+			{data.map((obj) => (
+				<div>
+					<img src={`${obj.img}`} alt={obj.cd} />
+					<div>
+						<div>
+							<p>{obj.cd}</p>
+							<p>Album - {obj.artist}</p>
+						</div>
+					</div>
+				</div>
+			))}
 		</div>
 	);
 }
